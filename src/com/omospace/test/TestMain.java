56b4e754b6fec021ccd702c618f0af095543032d
luckymanomo <luckymanomo@gmail.com>
2016-03-23 15:49:59 +0700

    c

--- src/com/omospace/test/TestMain.java
+++ src/com/omospace/test/TestMain.java
@@ -2,6 +2,7 @@ package com.omospace.test;
 
 import java.io.File;
 
+
 public class TestMain {
 	public static void main(String[] args){
 		File file=new File("D:/java_source_code/LEADs/fitdogcat/WebContent/resources/aceJs");
@@ -9,7 +10,9 @@ public class TestMain {
 		for(File subF:fileList){
 			if(subF.getName().endsWith(".js")){
 				System.out.println("<script src=\"<c:url value=\"/resources/aceJs/"+subF.getName()+"\" />\"></script>");
-			}
+			}//test pongsakorn jae kaa kuy
+			long test=323232;
+			new java.text.DecimalFormat("0.00").format(test);
 		}
 	}
 }
